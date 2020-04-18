4c206b969e1ed832f921656c969917dc0b77081b
Chen Gazit <gazit.chen@gmail.com>
2020-04-19 00:30:30 +0300

    mongoose close connection

--- src/db/mongooseConfig.js
+++ src/db/mongooseConfig.js
@@ -6,3 +6,14 @@ mongoose.connect(process.env.MONGODB_URL, {
     useCreateIndex: true,
     useFindAndModify: false
 });
+
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log(
+            termination(
+                'Mongoose default connection is disconnected due to application termination'
+            )
+        );
+        process.exit(0);
+    });
+});
